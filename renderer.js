af6278b9171dd8c3d5d562102242f9e40ff5138a
daffa1602 <142196820+daffa1602@users.noreply.github.com>
2023-11-29 11:02:43 +0700

    Add files via upload

---
+++ renderer.js
@@ -0,0 +1,8 @@
+// renderer.js
+document.addEventListener('DOMContentLoaded', function () {
+    const heading = document.createElement('h2');
+    heading.textContent = 'Aplikasi Resep Masakan Nusantara';
+    document.body.appendChild(heading);
+    
+    // Tambahkan logika aplikasi atau elemen HTML lainnya 
+  });
